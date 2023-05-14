39304d5294e8f51cf541e2b610e4a43ee6963ae6
garoskull8 <edgartrejoipn@gmail.com>
2023-05-14 16:30:30 -0600

    [UPDATE]mongodb_connection_created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadFileModule } from './modules/upload_file/upload_file.module';
-import { DatabaseModule } from './modules/database/database/database.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 
 @Module({
